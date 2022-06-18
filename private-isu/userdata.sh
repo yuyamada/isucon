a3c706886e7de1c17995ceb26180bc67fcece166
Yu Yamada <yuyamada@mbp2019.local>
2022-06-18 10:38:47 +0900

    fix userdata

--- private-isu/userdata.sh
+++ private-isu/userdata.sh
@@ -5,12 +5,19 @@ curl https://github.com/yuyamada.keys >> /home/ubuntu/.ssh/authorized_keys
 curl https://github.com/soya2222.keys >> /home/ubuntu/.ssh/authorized_keys
 curl https://github.com/yutaro-t.keys >> /home/ubuntu/.ssh/authorized_keys
 
-# monitoring/profiling tools
-apt update && apt -y install percona-toolkit dstat alp
+# tool installation
+apt update
 
 # query-digester
+apt -y install percona-toolkit
 git clone https://github.com/kazeburo/query-digester.git /home/ubuntu/query-digester
 cd /home/ubuntu/query-digester && install query-digester /usr/local/bin
+
+# dstat
+apt -y install dstat
+
+# alp
+apt -y install alp
 
 # netdata
 curl https://my-netdata.io/kickstart.sh > /tmp/netdata-kickstart.sh && sh /tmp/netdata-kickstart.sh
