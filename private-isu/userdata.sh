b4b96ef0c9266b6f0af7df84c5453b26cdaeee19
Yu Yamada <yuyamada@mbp2019.local>
2022-06-18 10:13:43 +0900

    add netdata and dstat

--- private-isu/userdata.sh
+++ private-isu/userdata.sh
@@ -6,15 +6,17 @@ curl https://github.com/soya2222.keys >> /home/ubuntu/.ssh/authorized_keys
 curl https://github.com/yutaro-t.keys >> /home/ubuntu/.ssh/authorized_keys
 
 # query-digester
-sudo apt update && sudo apt -y install percona-toolkit
+apt update && apt -y install percona-toolkit
 git clone https://github.com/kazeburo/query-digester.git /home/ubuntu/query-digester
-cd /home/ubuntu/query-digester && sudo install query-digester /usr/local/bin
+cd /home/ubuntu/query-digester && install query-digester /usr/local/bin
 
 # netdata
+curl https://my-netdata.io/kickstart.sh > /tmp/netdata-kickstart.sh && sh /tmp/netdata-kickstart.sh
 
 # dstat
+apt -y install dstat
 
 # neovim
-sudo add-apt-repository ppa:neovim-ppa/stable && sudo apt -y install neovim
+add-apt-repository ppa:neovim-ppa/stable && apt -y install neovim
 git clone https://github.com/yuyamada/nvim-conf.git /home/ubuntu/.config/nvim
-sudo mkdir /root/.config && sudo ln -s /home/ubuntu/.config/nvim /root/.config/nvim
+mkdir /root/.config && ln -s /home/ubuntu/.config/nvim /root/.config/nvim
